99c6e391d3a36f0127244668fc20f61224338395
Naveen <allaparthinaveen@gmail.com>
2024-11-20 23:12:31 +0800

    updated' *

--- src/com/utils/CalculationUtils.java
+++ src/com/utils/CalculationUtils.java
@@ -1,6 +1,7 @@
 package com.utils;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class CalculationUtils {
 	/**
@@ -11,5 +12,18 @@ public class CalculationUtils {
 	 */
 	public static BigDecimal percentageOf(final BigDecimal of, final BigDecimal number){
 		return (of.divide(new BigDecimal(100))).multiply(number);
+	}
+	
+	/**
+	 * change = difference between two prices/values
+	 * @param one
+	 * @param two
+	 * @return (change/original)X100
+	 */
+	public static BigDecimal percentageOfChange(final BigDecimal one, final BigDecimal two){
+		BigDecimal change = one.compareTo(two) > 0 ? one.subtract(two) : two.subtract(one);
+		BigDecimal original = one.compareTo(two) > 0 ? two : one;
+		
+		return change.divide(original, RoundingMode.CEILING).multiply(new BigDecimal(100));
 	}
 }
